7fa2e7c7ed6fb76cf5d43d5c6acf9ceca700ea7c
Kevin Hekert <kevin@hekert.net>
2024-06-19 16:39:44 +0200

    Support worker name (1.09)

--- miner/run.sh
+++ miner/run.sh
@@ -6,12 +6,14 @@ bashio::log.info "Initialiseren."
 CONFIG_PATH=/data/options.json
 cat $CONFIG_PATH
 
+
 CORES="$(bashio::config 'maximum_amount_of_used_cores')"
 WALLET="$(bashio::config 'wallet')"
 SERVER="$(bashio::config 'pool_server')"
+WORKER="$(bashio::config 'worker_name')"
+
+
 bashio::log.info "Starting with $CORES cores."
 bashio::log.info "Used server: $SERVER."
 bashio::log.info "Wallet: $WALLET."
-
-
-exec /xmrig/build/xmrig -o $SERVER -u $WALLET -t $CORES -k
+exec /xmrig/build/xmrig -o $SERVER -u $WALLET -t $CORES -k --randomx-mode light --coin monero -p WORKER
