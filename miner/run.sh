c155a02af2a111c7a3ee8ccd0deda73ba37e5751
Kevin Hekert <kevin@hekert.net>
2024-06-19 15:13:01 +0200

    Configurable run -> 1.0.6

--- miner/run.sh
+++ miner/run.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-bashio::log.info "Test."
+bashio::log.info "Initialiseren."
+
 CONFIG_PATH=/data/options.json
 cat $CONFIG_PATH
 
 CORES="$(bashio::config 'maximum_amount_of_used_cores')"
-echo $CORES
+WALLET="$(bashio::config 'wallet')"
+SERVER="$(bashio::config 'pool_server')"
+bashio::log.info "Starting with $CORES cores."
+bashio::log.info "Used server: $SERVER."
+bashio::log.info "Wallet: $WALLET."
+
+
+exec /xmrig/xmrig -o $SERVER -u $WALLET -t $CORES -k
